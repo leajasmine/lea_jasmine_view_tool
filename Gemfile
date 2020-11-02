079d5035d3915539c52576beb2615a58e5bf840d
lea jasmine <leajasmine@protonmail.com>
2020-11-02 13:32:26 -0500

    dev tool initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in Lea_Jasmine_View_tool.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/Lea_Jasmine_View_tool.rb
@@ -0,0 +1,8 @@
+require "Lea_Jasmine_View_tool/version"
+require "Lea_Jasmine_View_tool/renderer"
+
+module LeaJasmineViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
